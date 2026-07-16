79ab3e9eb3588de9df61e91acf98be428b147d18
lea <muehlberg.lea@gmail.com>
2026-07-16 14:21:38 +0200

    added calender card

---
+++ tracker/app/components/CalenderCard.tsx
@@ -0,0 +1,10 @@
+export default function CalenderCard() {
+	return (
+		<div className="calender-card">
+			<h2>July 2026</h2>
+			<div className="calender-grid">
+				{/*Days*/}
+			</div>
+		</div>
+	);
+}
